ece1fd5e7a8bee784a512f2de88795b38488627f
atvta <atvta@blackyak.com>
2023-04-27 16:46:53 +0900

    SecurityConfig 파일에 SecurityFilterChain bean과 CorsConfigurationSource bean 설정

---
+++ blackyakb2c/src/main/java/com/blackyak/b2c/common/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.blackyak.b2c.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer{
+	
+}
